3d73ffd0eba1819434324b49b14c7ad9af0d327c
chinasa1 <chinasa1@hotmail.com>
2019-04-28 08:57:07 +0200

    Add second plot

---
+++ plot2.R
@@ -0,0 +1,36 @@
+##Plot 1
+
+
+
+#Download the file and extract the file if not exist
+dataFile <- "household_power_consumption.txt"
+if(!file.exists()){
+  #)Download the file
+  fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  
+  download.file(url = fileUrl, destfile = "power_consumption.zip", method = "curl")
+  
+  #2)Unzip the file
+  unzip( "power_consumption.zip")
+  
+  #)Delete the zip file
+  file.remove("power_consumption.zip")
+  
+}
+
+#)Read the data
+powerCons <- read.table(file = "household_power_consumption.txt", na.strings = "?", sep = ";", header = TRUE)
+
+#)Subset the variable
+powerCons <- powerCons[(powerCons$Date=="1/2/2007" | powerCons$Date == "2/2/2007"),]
+
+
+#) Add new column datetime in posis
+powerCons$DateTime <- as.POSIXct(strptime(paste(as.Date(powerCons$Date, "%d/%m/%Y"), powerCons$Time), "%Y-%m-%d %H:%M:%S"))
+
+#)Plot
+plot(y=powerCons$Global_active_power, x=powerCons$DateTime, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+
+#Save the file
+dev.copy(png, "plot2.png")
+dev.off()
